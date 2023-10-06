982b2cac6c196a3a7c69adda3cfbf8dc523c0d66
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-10-06 13:06:41 -0300

    Implementa edição de título e descrição de tarefas já criadas

--- scripts/lib.js
+++ scripts/lib.js
@@ -1,3 +1,7 @@
 const removeTask = function(el){
     formTaskList.removeChild(el.parentElement)
 }
+function focusOut(ev){
+    ev.target.innerText = ev.target.querySelector("input").value
+    ev.target.innerHTML = ``
+}
--- scripts/task.js
+++ scripts/task.js
@@ -1,48 +1,64 @@
-class Task{
+class Task {
 
     #title;
     #description;
     #objectives;
     #creationDate;
 
-    constructor(title, description, objectives, date = new Date()){
-        
+    constructor(title, description, objectives, date = new Date()) {
+
         this.#title = title;
         this.#description = description;
         this.#objectives = objectives;
         this.#creationDate = typeof date === 'string' ? new Date(date) : date;
 
-        if (typeof objectives === 'string'){
+        if (typeof objectives === 'string') {
             this.#objectives = JSON.parse(objectives);
         }
 
     }
 
-    get title() {return this.#title};
-    get description() {return this.#description};
-    get objectives() {return this.#objectives};
-    get creationDate() {return this.#creationDate};
+    get title() {
+        return this.#title
+    };
+    get description() {
+        return this.#description
+    };
+    get objectives() {
+        return this.#objectives
+    };
+    get creationDate() {
+        return this.#creationDate
+    };
 
-    set title(newTitle) { this.#title = newTitle };
-    set description(newDescription) { this.#description = newDescription};
-    set objectives(newObjectives) { this.#objectives = newObjectives };
+    set title(newTitle) {
+        this.#title = newTitle;
+        storeTasks();
+    };
+    set description(newDescription) {
+        this.#description = newDescription;
+        storeTasks();
+    };
+    set objectives(newObjectives) {
+        this.#objectives = newObjectives
+    };
 
     /**
      * Transforms a Javascript Object into an Article HTMLElement
      * @returns HTMLElement
      */
-    createTaskElement(){
+    createTaskElement() {
         const article = document.createElement('article');
         article.classList.add('aside-task-article');
-        article.innerHTML = 
-        `
+        article.innerHTML =
+            `
         <div class="relative-wrapper"><img role="button" onclick="openDeleteDialog(this)" class="close-img" src="./assets/close.svg"></div>
         <h2><span>${this.#title}</span><span class="font-light font-75">${this.#creationDate.toLocaleString()}</span></h2>
         <p>${this.#description}</p>
         <ul></ul>
         `
 
-        this.#objectives.map((val, index) => {
+        this.objectives.map((val, index) => {
             const objective = document.createElement('li');
             objective.appendChild(document.createElement('input'));
             objective.firstChild.type = 'checkbox';
@@ -52,8 +68,33 @@ class Task{
             article.querySelector("ul").appendChild(objective);
         })
 
-        article.onmouseenter = () => {article.querySelector("img").classList.add("opacity-60")};
-        article.onmouseleave = () => {article.querySelector("img").classList.remove("opacity-60")};
+        article.onmouseenter = () => {
+            article.querySelector("img").classList.add("opacity-60")
+        };
+        article.onmouseleave = () => {
+            article.querySelector("img").classList.remove("opacity-60")
+        };
+
+        article.querySelector("h2 > span:first-child").onclick = (ev) => {
+            const editValue = ev.target;
+            editValue.innerHTML = `<input type="text" value="${ev.target.innerText}">`
+            editValue.onblur = (ev) => {
+
+                article.querySelector("h2 > span:first-child").innerHTML = ev.target.value;
+                this.title = ev.target.value;
+            }
+        }
+
+        article.querySelector("p").onclick = (ev) => {
+            const editValue = ev.target;
+            editValue.innerHTML = `<input type="text" value="${ev.target.innerText}">`
+            editValue.onblur = (ev) => {
+
+                article.querySelector("p").innerHTML = ev.target.value;
+                this.description = ev.target.value;
+            }
+        }
+
 
         return article;
 
@@ -63,8 +104,8 @@ class Task{
      * Returns all of the local tasks
      * @returns {Array<Task>}
      */
-    static getTasks(){
-        
+    static getTasks() {
+
         const tasksArray = [];
 
         document.querySelectorAll("aside article.aside-task-article").forEach((el) => {
@@ -76,7 +117,10 @@ class Task{
             el.querySelectorAll("li").forEach((li) => {
                 const title = li.innerText;
                 const checked = li.querySelector('input[type="checkbox"]').checked;
-                objectives.push({title, checked})
+                objectives.push({
+                    title,
+                    checked
+                })
             });
 
             tasksArray.push(new Task(title, description, objectives, date))
